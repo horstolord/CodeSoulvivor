259ce40f54853d1f968d64db7a303a599a8ef4e3
horstolord <horstolord123@googlemail.com>
2026-07-22 00:03:26 +0200

    Magic 1.0
    BABY!

---
+++ Systems/SpellTestComponent.cs
@@ -0,0 +1,57 @@
+using Sandbox;
+using Sandbox.Code.World;
+
+namespace Sandbox.Code.Systems;
+
+public sealed class SpellTestComponent : Component
+{
+	[Property] public SpellComponent SpellComp { get; set; }
+	[Property] public string CurrentPreset { get; set; } = "fireball";
+
+	protected override void OnStart()
+	{
+		SpellComp ??= Components.GetOrCreate<SpellComponent>();
+		LoadPreset( CurrentPreset );
+	}
+
+	public void LoadPreset( string presetName )
+	{
+		CurrentPreset = presetName;
+		if ( SpellComp != null )
+		{
+			SpellComp.RuneSlots = RuneLibrary.GetPreset( presetName );
+			Log.Info( $"[SpellTester] Equipped preset: '{presetName}' ({SpellComp.RuneSlots.Count} runes)" );
+		}
+	}
+
+	protected override void OnUpdate()
+	{
+		// Hotkeys to switch presets on the fly
+		if ( Input.Keyboard.Pressed( "1" ) ) LoadPreset( "fireball" );
+		if ( Input.Keyboard.Pressed( "2" ) ) LoadPreset( "empowered_fireball" );
+		if ( Input.Keyboard.Pressed( "3" ) ) LoadPreset( "dual_frost_beam" );
+		if ( Input.Keyboard.Pressed( "4" ) ) LoadPreset( "cluster_bomb" );
+		if ( Input.Keyboard.Pressed( "5" ) ) LoadPreset( "raw_force" );
+
+		// Cast on Right Click (mouse2) or 'E' key
+		if ( Input.Keyboard.Pressed( "mouse2" ) || Input.Keyboard.Pressed( "E" ) )
+		{
+			CastCurrentSpell();
+		}
+	}
+
+	public void CastCurrentSpell()
+	{
+		if ( SpellComp == null ) return;
+
+		var cameraRot = Scene.Camera?.WorldRotation ?? GameObject.WorldRotation;
+		var cameraPos = Scene.Camera?.WorldPosition ?? (GameObject.WorldPosition + Vector3.Up * 50f);
+		var aimDir = cameraRot.Forward;
+
+		bool castResult = SpellComp.CastSpell( cameraPos + aimDir * 30f, aimDir );
+		if ( castResult )
+		{
+			Log.Info( $"[SpellTester] Cast spell preset '{CurrentPreset}' successfully!" );
+		}
+	}
+}
---
+++ World/RuneLibrary.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using Sandbox.Code.Systems;
+
+namespace Sandbox.Code.World;
+
+public static class RuneLibrary
+{
+	public static readonly RuneDef FireForce = new RuneDef
+	{
+		Id = "fire_force",
+		DisplayName = "Fire Force",
+		Category = RuneCategory.Force,
+		BasePower = 25f,
+		EnergyCost = 5f,
+		ElementTag = RuneElementTag.Fire
+	};
+
+	public static readonly RuneDef FrostForce = new RuneDef
+	{
+		Id = "frost_force",
+		DisplayName = "Frost Force",
+		Category = RuneCategory.Force,
+		BasePower = 18f,
+		EnergyCost = 4f,
+		ElementTag = RuneElementTag.Frost
+	};
+
+	public static readonly RuneDef ProjectileMethod = new RuneDef
+	{
+		Id = "method_projectile",
+		DisplayName = "Projectile Method",
+		Category = RuneCategory.Method,
+		DeliveryType = RuneDeliveryType.Projectile,
+		CastDelay = 0.15f,
+		EnergyCost = 2f,
+		ProjectileTemplate = new ProjectileTemplate { Speed = 1200f, Lifetime = 4f }
+	};
+
+	public static readonly RuneDef BeamMethod = new RuneDef
+	{
+		Id = "method_beam",
+		DisplayName = "Beam Method",
+		Category = RuneCategory.Method,
+		DeliveryType = RuneDeliveryType.Beam,
+		Range = 1500f,
+		CastDelay = 0.10f,
+		EnergyCost = 3f
+	};
+
+	public static readonly RuneDef EmpowerModifier = new RuneDef
+	{
+		Id = "mod_empower",
+		DisplayName = "Empower",
+		Category = RuneCategory.Modifier,
+		EnergyCost = 5f,
+		ModifierEffect = ( ctx ) => { ctx.DamageMultiplier *= 1.5f; }
+	};
+
+	public static readonly RuneDef DualCast = new RuneDef
+	{
+		Id = "mod_dualcast",
+		DisplayName = "Dual Cast",
+		Category = RuneCategory.Multicast,
+		MulticastDrawCount = 2,
+		EnergyCost = 8f,
+		ModifierEffect = ( ctx ) => { ctx.SpreadAngle += 15f; }
+	};
+
+	public static readonly RuneDef ClusterTrigger = new RuneDef
+	{
+		Id = "trigger_cluster",
+		DisplayName = "Explosive Trigger",
+		Category = RuneCategory.Trigger,
+		EnergyCost = 10f,
+		TriggerNestedRunes = new List<RuneDef>
+		{
+			FireForce,
+			new RuneDef
+			{
+				Id = "sub_nova",
+				Category = RuneCategory.Method,
+				DeliveryType = RuneDeliveryType.SelfTouch,
+				AoERadius = 200f
+			}
+		}
+	};
+
+	public static List<RuneDef> GetPreset( string presetName )
+	{
+		return presetName.ToLower() switch
+		{
+			"fireball" => new List<RuneDef> { FireForce, ProjectileMethod },
+			"empowered_fireball" => new List<RuneDef> { EmpowerModifier, FireForce, ProjectileMethod },
+			"dual_frost_beam" => new List<RuneDef> { DualCast, FrostForce, BeamMethod },
+			"cluster_bomb" => new List<RuneDef> { ClusterTrigger, FireForce, ProjectileMethod },
+			"raw_force" => new List<RuneDef> { FireForce },
+			_ => new List<RuneDef> { FireForce, ProjectileMethod }
+		};
+	}
+}
